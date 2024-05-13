b0a5ee5e2c24eef7c31d324278b5d4f9f6cbbc26
EmptinessOfNature <lipengyubz@163.com>
2024-05-13 19:25:15 +0800

    完成server端每分钟下载实时数据并保存csv

--- server_main.py
+++ server_main.py
@@ -4,6 +4,15 @@ from ib_client_fufei import SimpleClient
 from ib_client_fufei import Contract
 import datetime
 import pandas as pd
+import os
+
+def ib_2_csv(hist_data):
+    lines=[['dt','open','close','high','low','vol']]
+    for i in range(hist_data.__len__()):
+        lines.append([hist_data[i].date[0:17],client.hist_data[i].open,client.hist_data[i].close,client.hist_data[i].high,client.hist_data[i].low,str(client.hist_data[i].volume)])
+    ret = pd.DataFrame(lines)
+    return ret
+
 
 client = SimpleClient("127.0.0.1", 7497, 5)
 time.sleep(3)
@@ -19,11 +28,14 @@ while True:
     contract.exchange = "SMART"
     now = datetime.datetime.now().strftime("%Y%m%d %H:%M:%S")
     req_id = 100
-    client.reqHistoricalData(
-        req_id, contract, now, "1 w", "1 min", "TRADES", False, 1, False, []
-    )
+    file_2_write = './data_server/'+now.replace(' ','').replace(':','')[0:8]+'_'+now.replace(' ','').replace(':','')[8:12]+'.csv'
+    if not os.path.exists(file_2_write):
+        client.reqHistoricalData(
+            req_id, contract, now, "1 w", "1 min", "TRADES", False, 1, False, []
+        )
+        time.sleep(3)
+        ret = ib_2_csv(client.hist_data)
+        ret.to_csv(file_2_write)
+        print(file_2_write,'保存完成！')
 
-    file_2_write = './data_server/'+now.replace(' ','').replace(':','')+'txt'
-    with open(file_2_write,'w') as f:
-        f.write(str(client.hist_data))
 
